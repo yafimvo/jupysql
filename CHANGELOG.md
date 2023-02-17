af6cf12a4297fb5e5b8a6b24ead91773e46c8a4b
Tony Kuo <yen.c@ploomber.io>
2023-02-17 11:49:03 -0500

    Add: CHANGE LOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## 0.5.7dev
 
+* [Fix] Addresses enable AUTOCOMMIT config issue in PostgreSQL ([#90](https://github.com/ploomber/jupysql/issues/90))
+
 ## 0.5.6 (2023-02-16)
 
 * [Feature] Shows missing driver package suggestion message ([#124](https://github.com/ploomber/jupysql/issues/124))
--- src/tests/integration/test_postgreSQL.py
+++ src/tests/integration/test_postgreSQL.py
@@ -6,7 +6,6 @@ def test_meta_cmd_display(ip_with_postgreSQL):
     assert ("public", "taxi", "table", "ploomber_app") in out.result
 
 
-# Known issue, addressing in https://github.com/ploomber/jupysql/issues/90
 def test_auto_commit_mode_on(ip_with_postgreSQL, capsys):
     ip_with_postgreSQL.run_cell("%config SqlMagic.autocommit=True")
     ip_with_postgreSQL.run_cell("%sql CREATE DATABASE new_db")
