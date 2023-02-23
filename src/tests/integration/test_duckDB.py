f749b1477ea948a899ec6ac2b85ff6fa53560c84
Tony Kuo <yen.c@ploomber.io>
2023-02-23 00:28:33 -0500

    Add: test for autocommit=False

--- src/tests/integration/test_duckDB.py
+++ src/tests/integration/test_duckDB.py
@@ -3,3 +3,13 @@ def test_auto_commit_mode_on(ip_with_duckDB, capsys):
     ip_with_duckDB.run_cell("%sql CREATE TABLE weather (city VARCHAR,);")
     out, _ = capsys.readouterr()
     assert "The database driver doesn't support such AUTOCOMMIT execution option" in out
+
+
+def test_auto_commit_mode_off(ip_with_duckDB, capsys):
+    ip_with_duckDB.run_cell("%config SqlMagic.autocommit=False")
+    ip_with_duckDB.run_cell("%sql CREATE TABLE weather (city VARCHAR,);")
+    out, _ = capsys.readouterr()
+    assert (
+        "The database driver doesn't support such AUTOCOMMIT execution option"
+        not in out
+    )
