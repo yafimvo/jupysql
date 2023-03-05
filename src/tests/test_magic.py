bc69c1e428715c3f3a75d154e1c9c2fe99c21ba1
yafimvo <yafimvo@gmail.com>
2023-03-05 18:51:29 +0200

    lint

--- src/tests/test_magic.py
+++ src/tests/test_magic.py
@@ -587,6 +587,10 @@ def test_error_on_invalid_connection_string_with_possible_typo(ip_empty, clean_c
 
 def test_jupysql_alias():
     assert SqlMagic.magics == {
-        "line": {'interactive_test': 'interactive_test', 'jupysql': 'execute', 'sql': 'execute'},
+        "line": {
+            "interactive_test": "interactive_test",
+            "jupysql": "execute",
+            "sql": "execute",
+        },
         "cell": {"jupysql": "execute", "sql": "execute"},
     }
