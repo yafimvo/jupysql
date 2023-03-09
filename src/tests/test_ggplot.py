b30d31be5376b1200ccb389464700f0a50f26e34
yafimvo <yafimvo@gmail.com>
2023-03-09 15:35:27 +0200

    lint

--- src/tests/test_ggplot.py
+++ src/tests/test_ggplot.py
@@ -22,7 +22,8 @@ def yellow_tripdata(tmp_empty):
     from pathlib import Path
     from urllib.request import urlretrieve
 
-    url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
+    url = "https://d37ci6vzurychx.cloudfront.net/trip-data/" \
+        "yellow_tripdata_2021-01.parquet"
 
     if not Path("yellow_tripdata_2021-01.parquet").is_file():
         urlretrieve(url, "yellow_tripdata_2021-01.parquet")
@@ -30,35 +31,53 @@ def yellow_tripdata(tmp_empty):
 
 @image_comparison(baseline_images=["boxplot"])
 def test_ggplot_geom_boxplot(yellow_tripdata):
-    ggplot(table="yellow_tripdata_2021-01.parquet", conn=conn) + \
-        aes(x="trip_distance") + geom_boxplot()
+    (
+        ggplot(table="yellow_tripdata_2021-01.parquet", conn=conn)
+        + aes(x="trip_distance")
+        + geom_boxplot()
+    )
 
 
 @image_comparison(baseline_images=["histogram_default"], extensions=["png"])
 def test_ggplot_geom_histogram(yellow_tripdata):
-    ggplot(table="yellow_tripdata_2021-01.parquet", conn=conn) + \
-        aes(x="trip_distance") + geom_histogram(bins=10, edgecolor="white")
+    (
+        ggplot(table="yellow_tripdata_2021-01.parquet", conn=conn)
+        + aes(x="trip_distance")
+        + geom_histogram(bins=10, edgecolor="white")
+    )
 
 
 @image_comparison(baseline_images=["histogram_with_default"])
 def test_ggplot_geom_histogram_with(short_trips_data):
-    ggplot(table="short-trips", with_="short-trips", conn=conn) + \
-        aes(x="trip_distance") + geom_histogram(bins=10)
+    (
+        ggplot(table="short-trips", with_="short-trips", conn=conn)
+        + aes(x="trip_distance")
+        + geom_histogram(bins=10)
+    )
 
 
 @image_comparison(baseline_images=["histogram_custom_edgecolor"], extensions=["png"])
 def test_ggplot_geom_histogram_edge_color(short_trips_data):
-    ggplot(table="short-trips", with_="short-trips", conn=conn) + \
-        aes(x="trip_distance") + geom_histogram(bins=10, edgecolor="white")
+    (
+        ggplot(table="short-trips", with_="short-trips", conn=conn)
+        + aes(x="trip_distance")
+        + geom_histogram(bins=10, edgecolor="white")
+    )
 
 
 @image_comparison(baseline_images=["histogram_custom_color"])
 def test_ggplot_geom_histogram_color(short_trips_data):
-    ggplot(table="short-trips", with_="short-trips", conn=conn) + \
-        aes(x="trip_distance") + geom_histogram(bins=10, color="red")
+    (
+        ggplot(table="short-trips", with_="short-trips", conn=conn)
+        + aes(x="trip_distance")
+        + geom_histogram(bins=10, color="red")
+    )
 
 
 @image_comparison(baseline_images=["histogram_custom_color_and_edge"])
 def test_ggplot_geom_histogram_color_and_edge(short_trips_data):
-    ggplot(table="short-trips", with_="short-trips", conn=conn) + \
-        aes(x="trip_distance") + geom_histogram(bins=10, color="red", edgecolor="#fff")
+    (
+        ggplot(table="short-trips", with_="short-trips", conn=conn)
+        + aes(x="trip_distance")
+        + geom_histogram(bins=10, color="red", edgecolor="#fff")
+    )
