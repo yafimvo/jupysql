a7fba4304fa8ed64391e85b6dc28cffc84ec0d4e
yafimvo <yafimvo@gmail.com>
2023-03-09 15:33:55 +0200

    yellow_tripdata added

--- src/tests/test_ggplot.py
+++ src/tests/test_ggplot.py
@@ -17,14 +17,25 @@ def short_trips_data(ip):
     )
 
 
+@pytest.fixture
+def yellow_tripdata(tmp_empty):
+    from pathlib import Path
+    from urllib.request import urlretrieve
+
+    url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2021-01.parquet"
+
+    if not Path("yellow_tripdata_2021-01.parquet").is_file():
+        urlretrieve(url, "yellow_tripdata_2021-01.parquet")
+
+
 @image_comparison(baseline_images=["boxplot"])
-def test_ggplot_geom_boxplot():
+def test_ggplot_geom_boxplot(yellow_tripdata):
     ggplot(table="yellow_tripdata_2021-01.parquet", conn=conn) + \
         aes(x="trip_distance") + geom_boxplot()
 
 
 @image_comparison(baseline_images=["histogram_default"], extensions=["png"])
-def test_ggplot_geom_histogram():
+def test_ggplot_geom_histogram(yellow_tripdata):
     ggplot(table="yellow_tripdata_2021-01.parquet", conn=conn) + \
         aes(x="trip_distance") + geom_histogram(bins=10, edgecolor="white")
 
