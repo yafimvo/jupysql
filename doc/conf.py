e9819c9e367da0ac6f5e5952e1b6f063423e7ecc
yafimvo <yafimvo@gmail.com>
2023-04-12 19:44:19 +0300

    questdb example added to docs

--- doc/conf.py
+++ doc/conf.py
@@ -22,6 +22,7 @@ nb_execution_excludepatterns = [
     "integrations/mariadb.ipynb",
     "integrations/clickhouse.ipynb",
     "integrations/mindsdb.ipynb",
+    "integrations/custom-connection.ipynb",
 ]
 nb_execution_in_temp = True
 nb_execution_show_tb = True
