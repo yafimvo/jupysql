a2825dabccc90aa7a4701a2dbb029a126243523d
yafimvo <yafimvo@gmail.com>
2023-04-17 11:55:40 +0300

    index updated

--- doc/_static/algolia.js
+++ doc/_static/algolia.js
@@ -1,10 +1,13 @@
 docsearch({
     appId: 'Y6L7HQ2HZO',
     apiKey: '9a1fd3379e6d318ef4f46aa36a3c5fe6',
-    indexName: 'crawler_JupySQL',
+    indexName: 'ploomber_jupysql',
     // Replace inputSelector with a CSS selector
     // matching your search input
     inputSelector: 'input[type=search]#search-input',
     // Set debug to true if you want to inspect the dropdown
-    debug: false
+    debug: false,
+    searchParameters : {
+      hitsPerPage: 10
+    }
   });
